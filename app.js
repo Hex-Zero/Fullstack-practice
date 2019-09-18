da4bdd9f90c768a93f0b7f3030478d07ee72f0f0
Hex-Zero <hex0@live.com>
2019-09-18 15:54:45 +0100

    Server started

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery{
+            events:[String!]!
+        }
+        type RootMutation{
+            createEvent(name: String): String
+        }
+        schema{
+         query:RootQuery
+            mutation:RootMutation
+        }`),
+    rootValue: {
+      events: () => {
+        return ["Cooking", "Sailing", "All-Night Coding"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
+
+app.listen(3000);
